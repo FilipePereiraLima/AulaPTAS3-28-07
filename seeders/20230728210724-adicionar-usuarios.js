d4a1f78a9d36c29116fc0b77a6605183e7240cdc
FilipePereiraLima <filipe.lima4@estudante.ifms.edu.br>
2023-08-04 01:18:19 -0300

    Adicionando os arquivos

---
+++ seeders/20230728210724-adicionar-usuarios.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+     
+    await queryInterface.bulkInsert('usuarios', [{
+      nome: 'John Doe'
+    }], {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+   
+    await queryInterface.bulkDelete('Usuarios', null, {});
+     
+  }
+};
